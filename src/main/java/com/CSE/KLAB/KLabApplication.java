58b5e45e8f974f38cb3fae1ba86c95a17efd3d97
ulsandonghun <donghoon141@naver.com>
2024-05-04 17:32:33 +0900

    [set] : 서버 세팅

---
+++ src/main/java/com/CSE/KLAB/KLabApplication.java
@@ -0,0 +1,13 @@
+package com.CSE.KLAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CSE/KLAB/KLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSE.KLAB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
