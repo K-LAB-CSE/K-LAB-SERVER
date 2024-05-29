4cacfdc17666dcb6447f65d0848a5fe52ea04410
ulsandonghun <donghoon141@naver.com>
2024-05-29 15:31:15 +0900

    [feat] : 네덜란드어, 한국어 일기 생성 기능 구현

--- src/main/java/com/CSE/KLAB/DiaryGenerate/Configuration/GPTController.java
+++ src/main/java/com/CSE/KLAB/DiaryGenerate/Configuration/GPTController.java
@@ -22,13 +22,15 @@ public class GPTController {
     private String apiUrl;
     private final RestTemplate restTemplate;
 
-    @PostMapping("/diary")
-    public String generateDiary(
+
+    // 한국어 일기 생성 컨트롤러
+    @PostMapping("/diary/korean")
+    public String generateKoreanDiary(
             @RequestParam String username,
             @RequestParam String userInput,
             @RequestParam String todayDate) {
 
-        // 프롬프트 문자열 생성
+        // 일기 생성 프롬프트 초기화
         StringBuilder promptBuilder = new StringBuilder();
         promptBuilder.append("This system is ai auto writing diary\n");
         promptBuilder.append("Written person name is ").append(username).append(", write name at the end of diary.\n");
@@ -45,6 +47,55 @@ public class GPTController {
         GPTResponse gptResponse = restTemplate.postForObject(
                 apiUrl, gptRequest, GPTResponse.class);
 
-        return gptResponse.getChoices().get(0).getMessage().getContent();
+        // 한국어 일기 생성 프롬프트 초기화
+        StringBuilder promptKoreanBuilder = new StringBuilder();
+        promptKoreanBuilder.append("translate this diary in korean language: ").append(gptResponse.getChoices().get(0).getMessage().getContent()).append("\n");
+        promptKoreanBuilder.append("okay I will translate this diary in korean word!\n");
+
+        GPTRequest gptKoreanRequest = new GPTRequest(
+                model, promptKoreanBuilder.toString(), 1, 256, 1, 2, 2);
+
+        GPTResponse gptKoreanResponse = restTemplate.postForObject(
+                apiUrl, gptKoreanRequest, GPTResponse.class);
+
+        return gptKoreanResponse.getChoices().get(0).getMessage().getContent();
+    }
+
+    // 네덜란드어 일기 생성 컨트롤러
+    @PostMapping("/diary/dutch")
+    public String generateDutchDiary(
+            @RequestParam String username,
+            @RequestParam String userInput,
+            @RequestParam String todayDate) {
+
+        // 일기 생성 프롬프트 초기화
+        StringBuilder promptBuilder = new StringBuilder();
+        promptBuilder.append("This system is ai auto writing diary\n");
+        promptBuilder.append("Written person name is ").append(username).append(", write name at the end of diary.\n");
+        promptBuilder.append("I want you to write diary with information ").append(userInput).append("\n");
+        promptBuilder.append("okay is there any information i need to know?\n");
+        promptBuilder.append("Also, write down date with information, ").append(todayDate).append("\n");
+        promptBuilder.append("okay is there any information i need to know?\n");
+        promptBuilder.append("In the diary, change lines every 20 words.\n");
+        promptBuilder.append("okay is there any information i need to know?\n");
+
+        GPTRequest gptRequest = new GPTRequest(
+                model, promptBuilder.toString(), 1, 256, 1, 2, 2);
+
+        GPTResponse gptResponse = restTemplate.postForObject(
+                apiUrl, gptRequest, GPTResponse.class);
+
+        // 네덜란드어 일기 생성 프롬프트 초기화
+        StringBuilder promptDutchBuilder = new StringBuilder();
+        promptDutchBuilder.append("translate this diary in dutch language: ").append(gptResponse.getChoices().get(0).getMessage().getContent()).append("\n");
+        promptDutchBuilder.append("okay I will translate this diary in dutch word!\n");
+
+        GPTRequest gptDutchRequest = new GPTRequest(
+                model, promptDutchBuilder.toString(), 1, 256, 1, 2, 2);
+
+        GPTResponse gptDutchResponse = restTemplate.postForObject(
+                apiUrl, gptDutchRequest, GPTResponse.class);
+
+        return gptDutchResponse.getChoices().get(0).getMessage().getContent();
     }
 }
